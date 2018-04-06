23ae8cccd17cfbabcc9fc174beda4dad436e6941
Tony <>
2018-04-06 18:07:24 +0700

    Add comment

--- frontend/mobile/MovieOnline/app/src/main/java/com/neptune/movieonline/activities/MovieDetailActivity.java
+++ frontend/mobile/MovieOnline/app/src/main/java/com/neptune/movieonline/activities/MovieDetailActivity.java
@@ -6,17 +6,22 @@ import android.support.v7.widget.RecyclerView;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.android.volley.Request;
 import com.android.volley.Response;
 import com.bumptech.glide.Glide;
 import com.neptune.movieonline.R;
 import com.neptune.movieonline.adapters.CommentListAdapter;
 import com.neptune.movieonline.models.Comment;
 import com.neptune.movieonline.models.Movie;
+import com.neptune.movieonline.utils.constants.Rest;
 import com.neptune.movieonline.utils.helpers.GlideHelper;
 import com.neptune.movieonline.utils.helpers.VolleyHelper;
+import com.neptune.movieonline.utils.requests.CommentRequest;
 import com.neptune.movieonline.utils.requests.GsonRequest;
 import com.neptune.movieonline.utils.requests.MovieRequest;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import butterknife.BindView;
@@ -64,5 +69,15 @@ public class MovieDetailActivity extends AppCompatActivity {
     }
 
     private void fetchComments() {
+        GsonRequest<Comment[]> commentRequest = CommentRequest.getAll(
+                new Response.Listener<Comment[]>() {
+                    @Override
+                    public void onResponse(Comment[] response) {
+                        commentList = new ArrayList<>(Arrays.asList(response));
+                        commentListAdapter = new CommentListAdapter(MovieDetailActivity.this, commentList);
+                        recyclerViewCommentList.setAdapter(commentListAdapter);
+                    }
+                }, null);
+        VolleyHelper.getInstance().addToRequestQueue(commentRequest);
     }
 }
--- frontend/mobile/MovieOnline/app/src/main/java/com/neptune/movieonline/adapters/CommentListAdapter.java
+++ frontend/mobile/MovieOnline/app/src/main/java/com/neptune/movieonline/adapters/CommentListAdapter.java
@@ -1,10 +1,17 @@
 package com.neptune.movieonline.adapters;
 
+import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 
+import com.neptune.movieonline.R;
 import com.neptune.movieonline.adapters.viewholders.CommentViewHolder;
+import com.neptune.movieonline.models.Comment;
+
+import java.util.List;
 
 /**
  * Created by Neptune on 3/28/2018.
@@ -12,15 +19,27 @@ import com.neptune.movieonline.adapters.viewholders.CommentViewHolder;
 
 public class CommentListAdapter extends RecyclerView.Adapter<CommentViewHolder> {
 
+    private Context context;
+    private List<Comment> commentList;
+
+    public CommentListAdapter(Context context, List<Comment> commentList) {
+        this.context = context;
+        this.commentList = commentList;
+    }
+
     @NonNull
     @Override
     public CommentViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View view = inflater.inflate(R.layout.item_comment, parent, false);
+        return new CommentViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull CommentViewHolder holder, int position) {
+        final Comment item = commentList.get(position);
 
+        holder.getTextViewContent().setText(item.getContent());
     }
 
     @Override
